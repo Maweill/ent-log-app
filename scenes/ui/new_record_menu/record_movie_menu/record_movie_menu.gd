7c768f3db04c8709d1e3197a65f3f325e79ed387
bednyakov <bednyakov99@gmail.com>
2023-08-05 07:50:32 +0300

    Кнопка сохранения блокируется, пока не будут заполнены все поля формы

--- scenes/ui/new_record_menu/record_movie_menu/record_movie_menu.gd
+++ scenes/ui/new_record_menu/record_movie_menu/record_movie_menu.gd
@@ -1,16 +1,26 @@
 extends BaseMenu
 
 @onready var _movie_name: LineEdit = %MovieName
+@onready var _save_button: Button = %Save
 
-var _is_like: bool
+var _is_like = null
+
+func _ready() -> void:
+	_save_button.disabled = true
+
+
+func _on_movie_name_text_changed(_new_text: String) -> void:
+		_try_enable_save_button()
 
 
 func _on_dislike_pressed() -> void:
 	_is_like = false
+	_try_enable_save_button()
 
 
 func _on_like_pressed() -> void:
 	_is_like = true
+	_try_enable_save_button()
 
 
 func _on_save_pressed() -> void:
@@ -19,3 +29,7 @@ func _on_save_pressed() -> void:
 	movie_model.is_like = _is_like
 	movie_model.movie_name = _movie_name.text
 	ModelStorage.add_model(movie_model)
+
+
+func _try_enable_save_button() -> void:
+	_save_button.disabled = _movie_name.text.is_empty() or _is_like == null
